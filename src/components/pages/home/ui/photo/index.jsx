9f88497b0fd66ece87d88813b9244500b9f3ff60
abduganii <abduganiabdu2@gmail.com>
2024-06-24 14:30:03 +0500

    fixed

--- src/components/pages/home/ui/photo/index.jsx
+++ src/components/pages/home/ui/photo/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React from "react";
 import Marquee from "react-fast-marquee";
 const arr = ["/2.jpg", "/1.jpg", "/2.jpg", "/3.jpg", "/2.jpg", "/1.jpg"];
-const arr1 = ["/4.jpg", "/5.jpg", "/6.jpg", "/4.jpg", "/5.jpg", "/6.jpg"];
+const arr1 = ["/4.jpg", "/5.jpg", "/6.jpg", "/5.jpg", "/4.jpg", "/5.jpg"];
 
 export default function PhoneSection() {
   return (
@@ -13,13 +13,13 @@ export default function PhoneSection() {
           Фотогорафии от сердце{" "}
         </h3>
       </Contianer>
-      <Marquee >
+      <Marquee>
         <div>
           <div className="flex h-[222px] gap-[8px] mb-[8px] ml-[8px]">
             {arr?.map((e, i) => (
               <Image
                 src={e}
-                className={`object-cover `}
+                className={`object-fill `}
                 width={i % 2 == 0 ? 368 : 737}
                 height={222}
                 objectFit={"cover"}
@@ -29,7 +29,7 @@ export default function PhoneSection() {
           <div className="flex h-[222px] gap-[8px] ">
             {arr1?.map((e, i) => (
               <Image
-                className={`object-cover `}
+                className={`object-fill `}
                 src={e}
                 width={i % 2 == 0 ? 737 : 368}
                 height={222}
