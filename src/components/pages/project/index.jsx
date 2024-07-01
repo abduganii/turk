ce874bca098a0fe99e5a28c2287139e7c1063bf5
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2024-07-01 19:31:40 +0500

    fixed

--- src/components/pages/project/index.jsx
+++ src/components/pages/project/index.jsx
@@ -1,28 +1,49 @@
-import Contianer from '@/components/ui/container'
-import Image from 'next/image'
-import {useState} from 'react'
-import AboutUs from './ui/about-us'
-import { useRouter } from 'next/router'
+import Container from '@/components/ui/container';
+import Image from 'next/image';
+import { useState } from 'react';
+import AboutUs from './ui/about-us';
+import { useRouter } from 'next/router';
+
 export default function ProductPage() {
-    const [image,setImage] = useState("/a.png")
-    const router = useRouter()
+  const [image, setImage] = useState('/a.png');
+  const router = useRouter();
+
+  const handleMouseMove = (event) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+
+    if (mouseX <= 400) {
+      setImage('/a-hover.png');
+    } else {
+      setImage('/a.png');
+    }
+  };
+
+  const handleClick = () => {
+    router.push('/project-one');
+  };
+
   return (
     <>
-        <div className='w-full '>
-              <Image 
-                 className='w-full pl-[50px] cursor-pointer max-w-[1414px] mx-auto  pt-[182px] mb-[62px]'
-                src={image} 
-                width={1414} 
-                height={811 }
-                onMouseEnter={()=> setImage("/a-hover.png")}
-                onMouseLeave={()=> setImage("/a.png")} 
-                onClick={()=>router.push('/project-one')}
-               /> 
-           
+      <div className="w-full">
+        <div
+          className="relative w-full max-w-[1414px] mx-auto pt-[182px] mb-[62px]"
+          style={{ paddingLeft: '50px' }}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={() => setImage('/a.png')}
+          onClick={handleClick}
+        >
+          <Image
+            className="w-full cursor-pointer"
+            src={image}
+            width={1414}
+            height={811}
+          />
         </div>
-          <Contianer>
-              <AboutUs/>
-          </Contianer>
+      </div>
+      <Container>
+        <AboutUs />
+      </Container>
     </>
-  )
+  );
 }
