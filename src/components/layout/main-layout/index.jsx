693b05c6f5fde7e72a2c42269e2f266bfc51eccb
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2024-07-01 19:38:15 +0500

    dsds

--- src/components/layout/main-layout/index.jsx
+++ src/components/layout/main-layout/index.jsx
@@ -1,14 +1,10 @@
 import Footer from '@/components/ui/footer'
 import Header from '@/components/ui/header'
-import { GetMe } from '@/serive/global'
-import React, { useEffect, useState } from 'react'
-import { useQuery } from 'react-query'
 
 export default function MainLayout({ children }) {
-  const { data: user } = useQuery(['user'], () => GetMe())
   return (
       <>
-      <Header user={user} />
+      <Header />
          {children}
         <Footer/>
     </>
